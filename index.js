f653f3e7afa6ab970a6616ba774e0464e0ea6645
GiulianoGosty <giuliano.gostinfini.93@gmail.com>
2025-10-19 15:37:52 +0200

    add graphql schema start first graphql queries

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import 'dotenv/config';
 import fastifyMysql from "@fastify/mysql";
 import contiRoutes from "./routes/conti.js";
 import operazioniRoutes from "./routes/operazioni.js";
+import schema from './schema.js'
 const fastify = Fastify({
   logger: true
 });
@@ -11,6 +12,29 @@ const { DB_USERNAME: dbUser, DB_PASSWORD: dbPass, DB_NAME: dbName, DB_HOST: dbHo
 fastify.get('/', function (request, response) {
  response.send({hello:'world'})
 })
+const resolvers = {
+  Query: {
+    hello: async()=>"hello with graphql",
+    operazioni: async () =>{
+
+      const connection = await fastify.mysql.getConnection();
+      const [rows, fields] = await connection.query(
+        'SELECT * FROM operazioni'
+      )
+      connection.release()
+      return rows
+    },
+    conti: async () =>{
+
+      const connection = await fastify.mysql.getConnection();
+      const [rows, fields] = await connection.query(
+        'SELECT * FROM conti'
+      )
+      connection.release()
+      return rows
+    },
+  }
+}
 
 fastify.register(contiRoutes);
 fastify.register(operazioniRoutes);
@@ -18,10 +42,17 @@ fastify.register(operazioniRoutes);
 fastify.register(fastifyMysql, {
   //why zed are you blocking my backthick
   connectionString: "mysql://"+dbUser+":"+dbPass+"@"+dbHost+"/"+dbName,
+  promise: true
+})
+fastify.register(import('mercurius'),{
+  schema,
+  resolvers,
+  graphiql:true
 })
 fastify.listen({ port: 3000 }, function (err, address) {
   if(err){
     fastify.log.error(err);
+    console.log(err)
     process.exit(1);
   }
   console.log(process.env)
---
+++ schema.js
@@ -0,0 +1,20 @@
+const schema = `
+type Conto {
+  ID: ID,
+  Nome: String,
+}
+
+type Operazione{
+  ID: ID,
+  Importo: Float,
+  Descrizione: String,
+  Conto_id: Int
+}
+
+type Query {
+  hello: String,
+  operazioni: [Operazione],
+  conti: [Conto]
+}
+`
+export default schema
